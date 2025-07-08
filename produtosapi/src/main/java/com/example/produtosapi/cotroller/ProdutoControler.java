2595dd0367715cc39d97ac685382bc9ef194e63d
Allyson de Araújo oliveira <allyson.deolliveira9@gmail.com>
2025-07-08 14:35:58 -0300

    Add files via upload

--- produtosapi/src/main/java/com/example/produtosapi/cotroller/ProdutoControler.java
+++ produtosapi/src/main/java/com/example/produtosapi/cotroller/ProdutoControler.java
@@ -1,20 +1,40 @@
 package com.example.produtosapi.cotroller;
 
 import com.example.produtosapi.model.Produto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("produtos")
 public class ProdutoControler {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoControler(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar(@RequestBody Produto produto) {
 
         System.out.println("Produto recebido: " + produto);
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        produtoRepository.save(produto);
 
         return produto;
     }
+
+    @GetMapping("{id}")
+    public Produto ObterPorId (@PathVariable("id") String id) {
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletar (@PathVariable("id") String id) {
+        produtoRepository.deleteById(id);
+    }
+
 }
